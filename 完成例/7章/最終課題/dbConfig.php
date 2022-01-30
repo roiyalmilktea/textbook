ae13b4ce0e79cdff0ecce3418c8da8289aaa4074
roiyal <roiyalmilktea123456789@gmail.com>
2022-01-30 23:53:42 +0900

    add

--- 完成例/7章/最終課題/dbConfig.php
+++ 完成例/7章/最終課題/dbConfig.php
@@ -1,6 +1,6 @@
 <?php
 define("DB_SERVER", "localhost");     // サーバ
-define("DB_NAME", "book");  // データベース
+define("DB_NAME", "book");            // データベース
 define("DB_USER", "jikkyo");          // ユーザ
 define("DB_PASS", "pass");            // パスワード
 ?>
--- 完成例/7章/最終課題/topmenu.php
+++ 完成例/7章/最終課題/topmenu.php
@@ -1,7 +1,9 @@
   <nav id="menu">
     <ul>
       <li><a href="./index.php">ホーム</a></li>
-      <li><a href="./roomList.php">お部屋紹介</a></li>
+      <li><a href="./bookList.php">紹介</a></li>
       <li><a href="./reserveDay.php">ご予約</a></li>
     </ul>
   </nav>
+
+  <!-- bookListとroomListの違いを見ながら進める -->
